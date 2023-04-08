55ea9ec1141cd596dde7e0a20c9d0ff817fdf276
wc-dev <chan.wilson.wc@gmail.com>
2023-04-08 16:02:21 -0700

    feat: added treesitter folding configuration with zc and zo

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -3,6 +3,13 @@ local M = {}
 
 -- Path to overriding theme and highlights files
 local highlights = require "custom.highlights"
+local opt = vim.opt
+local api = vim.api
+
+-- overriding options in core/init.lua
+opt.foldmethod = "expr"
+opt.foldexpr = "nvim_treesitter#foldexpr()"
+
 
 M.ui = {
   theme = "solarized_dark",
@@ -16,5 +23,28 @@ M.plugins = "custom.plugins"
 
 -- check core.mappings for table structure
 M.mappings = require "custom.mappings"
+
+
+-- autocmd for treesitter folding --
+function M.nvim_create_augroups(definitions)
+    for group_name, definition in pairs(definitions) do
+        api.nvim_command('augroup '..group_name)
+        api.nvim_command('autocmd!')
+        for _, def in ipairs(definition) do
+            local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
+            api.nvim_command(command)
+        end
+        api.nvim_command('augroup END')
+    end
+end
+
+local autoCommands = {
+    -- other autocommands
+    open_folds = {
+        {"BufReadPost,FileReadPost", "*", "normal zR"}
+    }
+}
+
+M.nvim_create_augroups(autoCommands)
 
 return M
